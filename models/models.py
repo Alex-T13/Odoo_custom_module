035788fd6811ea61c4fc1a330d1bf6bebb8c4738
Alexander-PC <morphix313@gmail.com>
2021-09-14 01:28:28 +0300

    create models

--- models/models.py
+++ models/models.py
@@ -16,3 +16,25 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+from odoo import models, fields, api
+
+
+class Manufacturer(models.Model):
+    _name = 'phones.manufacturer'
+#   _inherit = 'product.product'
+
+    manufacturer_name = fields.Char(string='Manufacturer', required=True)
+
+
+class Brand(models.Model):
+    _name = 'phones.brand'
+#    _inherit = 'product.product'
+
+    brand_name = fields.Char(string='Brand', required=True)
+    manufacturer_id = fields.Many2one(
+        comodel_name='manufacturer',
+        ondelete='cascade',
+        string='Manufacturer',
+        required=True
+    )
